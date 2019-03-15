36d759f219ed5f5f57cbcb6c9c4d52937cbcba0a
dsaha-84 <dsaha.tr02@gmail.com>
2019-03-15 08:40:47 +0530

    jdbc class add

---
+++ src/Service.java
@@ -0,0 +1,12 @@
+import java.sql.*;
+public class Service
+Connection con-null;
+ResultSet rs= null;
+Statement st= null;
+
+public List service()
+{
+	Class.forName();
+	con=DriverManager.fetConnection(jdbc:mysql://localhost:...);
+}
+
